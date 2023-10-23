9fca2feae1315aaa1a3293d4895995851d0c257d
Sergey Suvorov <suvorovsergey99@gmail.com>
2023-10-23 01:39:20 +0000

    feat: added elements count on home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { redirect } from "react-router-dom";
 import { useNavigate } from "react-router-dom"
+import routes from "../../routes.js";
+import { useEffect } from "react";
 
 import "./Home.css";
 
@@ -13,33 +15,48 @@ import OkNodesIcon from '../../Static/Icons/ok_nodes_icon.svg';
 const Home = () => {
   const navigate = useNavigate();
 
+  const [homeData, setHomeData] = React.useState({});
+
+  const dataFetch = async () => {
+    try {
+      const response = await fetch(routes.home);
+      setHomeData(await response.json());
+    } catch (error) {
+      console.log(error);
+    } 
+  };
+
+  useEffect(() => {
+    dataFetch();
+  }, []);
+
   return (
     <div className="home">
       <div className="buttons-container">
         <div className="menu-button services-button" onClick={() => navigate("/services")}>
           <ServicesIcon className="menu-button__icon"/>
           <span className="menu-button__title">Services</span>
-          {/* <span className="menu-button__count">14</span> */}
+          <span className="menu-button__count">{homeData?.services}</span>
         </div>
         <div className="menu-button groups-button" onClick={() => navigate("/groups")}>
           <GroupsIcon className="menu-button__icon "/>
           <span className="menu-button__title">GROUPS</span>
-          {/* <span className="menu-button__count">88</span> */}
+          <span className="menu-button__count">{homeData?.groups}</span>
         </div>
         <div className="menu-button all-nodes-button" onClick={() => navigate("/nodes/all")}>
           <ServerIcon className="menu-button__icon"/>
           <span className="menu-button__title">All nodes</span>
-          {/* <span className="menu-button__count">14</span> */}
+          <span className="menu-button__count">{homeData?.nodes}</span>
         </div>
         <div className="menu-button failed-nodes-button" onClick={() => navigate("nodes/failed")}>
           <FailedNodesIcon className="menu-button__icon" />
           <span className="menu-button__title">Failed</span>
-          {/* <span className="menu-button__count">69</span> */}
+          <span className="menu-button__count">{homeData?.failedNodes}</span>
         </div>
         <div className="menu-button ok-nodes-button" onClick={() => navigate("nodes/ok")}>
           <OkNodesIcon className="menu-button__icon "/>
           <span className="menu-button__title">OK</span>
-          {/* <span className="menu-button__count">123</span> */}
+          <span className="menu-button__count">{homeData?.okNodes}</span>
         </div>
       </div>
     </div>
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
+const baseURL = 'http://localhost:3000'
+
 const routes = {
-  node: (nodeId) => `http://localhost:3000/node/${nodeId}`,
-  nodes: (type) => `http://localhost:3000/nodes/${type}`,
-  services: 'http://localhost:3000/services',
-  groups: 'http://localhost:3000/groups',
-  createGroup: 'http://localhost:3000/groups/create',
-  deleteGroup: (groupId) => `http://localhost:3000/groups/delete/${groupId}`,
-  turnOffService: (id) => `http://localhost:3000/services/toggle/${id}`
+  home: `${baseURL}/home`,
+  node: (nodeId) => `${baseURL}/node/${nodeId}`,
+  nodes: (type) => `${baseURL}/nodes/${type}`,
+  services: `${baseURL}/services`,
+  groups: `${baseURL}/groups`,
+  createGroup: `${baseURL}/groups/create`,
+  deleteGroup: (groupId) => `${baseURL}/groups/delete/${groupId}`,
+  turnOffService: (id) => `${baseURL}/services/toggle/${id}`
 }
 
 export default routes;
